a9d47f25517d1c382f2fcd1c8404d87939455f0a
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-11-19 00:37:50 -0800

    Added ability to use heroku mysql database

--- connection/index.js
+++ connection/index.js
@@ -1,6 +1,10 @@
-module.exports =  require('mysql2').createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'groot',
-  database: 'cookies_db'
-})
+module.exports = require('mysql2')
+  .createConnection(process.env.JAWSDB_URL 
+    ? process.env.JAWSDB_URL 
+    : {
+      host: 'localhost',
+      user: 'root',
+      password: 'groot',
+      database: 'cookies_db'
+    }
+  )
