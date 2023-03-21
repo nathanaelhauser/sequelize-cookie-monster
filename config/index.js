b619a8e59fd24ec8c0d6bad78a23138d3d50b648
Nathanael Hauser <nathanaelhauser@gmail.com>
2023-03-20 19:54:58 -0700

    - Trying to deploy on heroku

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const Sequelize = require('sequelize')
 
 const SQL_URI = process.env.JAWSDB_URL || 'mysql://root:root@localhost/cookies_db';
 
-module.exports = new Sequelize(SQL_URI, { dialect: 'mysql' });
+module.exports = new Sequelize(SQL_URI);
