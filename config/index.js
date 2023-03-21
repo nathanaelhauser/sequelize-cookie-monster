22859c594f71ad3b5b0b4fcfae8905974ff3dc76
Nathanael Hauser <nathanaelhauser@gmail.com>
2023-03-20 18:33:23 -0700

    - Updated for AWS deployment

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
 
-const SQL_URI = process.env.MYSQLCONNSTR_jawsdb || 'mysql://root:root@localhost/cookies_db';
+const SQL_URI = process.env.JAWSDB_URL || 'mysql://root:root@localhost/cookies_db';
 
 module.exports = new Sequelize(SQL_URI, { dialect: 'mysql' });
